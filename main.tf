d513577e89d416978cc45d6de969bcd2a6364b9c
mustafagoek <mustafa.gok@deop.ca>
2023-02-28 23:38:00 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.45.0"
+    }
+  }
+}
+
+provider "azurerm" {
+      features  { 
+  }
+}
+resource "azurerm_resource_group" "rg1" {
+  name     = var.rg_name
+  location = var.location
+} # degiskenleri variables ile belirlemek basta en iyisi
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  address_space       = ["10.60.0.0/22"]
+
+}
+resource "azurerm_subnet" "subnet1" {
+  name                 = var.subnet1_name
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.60.0.0/24"]
+}
+resource "azurerm_subnet" "subnet2" {
+  name                 = var.subnet2_name
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.60.1.0/24"]
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "rg_name" {
+    default = "rg1"
+}
+variable "location" {
+    default ="UK South"
+}
+variable "vnet_name" {
+    default ="vnet"
+}
+variable "subnet1_name" {
+    default = "subnet1"
+}
+variable "subnet2_name" {
+    default = "subnet2"
+}
