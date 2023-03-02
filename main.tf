890576f519ee947995d7bfdbed2c496fb60d60b5
mustafagoek <mustafa.gok@deop.ca>
2023-03-02 12:40:21 +0100

    vm_module added

--- main.tf
+++ main.tf
@@ -35,3 +35,13 @@ resource "azurerm_subnet" "subnet2" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.60.1.0/24"]
 }
+
+module "vm1" {
+  source = " ./module/vm_module"
+  vm_name = "vm1"
+  resource_group_name = azurerm_resource_group.rg1.name
+  username = "mustafagok"
+  password = "Pasword123"
+  subnet_id = azurerm_subnet.subnet1.id
+
+}
---
+++ module/vm_module/variables.tf
@@ -0,0 +1,21 @@
+variable = "vm_name" {
+
+}
+variable = "resource_group" {
+
+}
+variable = "location" {
+    default = "East US"
+}
+variable = "username" {
+
+}
+variable = "password" {
+
+}
+variable = "subnet_id" {
+
+}
+
+# module klasöründe main.tf yazdiktan sonra icinde yazdigimiz var.resource lari variable lari 
+# burada belirliyoruz
